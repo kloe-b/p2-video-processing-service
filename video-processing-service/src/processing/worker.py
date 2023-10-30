c168834d63e656dcdab24c14ebda5c8ecf8d4ba2
kloena <burazerikloena@gmail.com>
2023-10-30 18:52:05 +0700

    connect to backend with redis

--- video-processing-service/src/processing/worker.py
+++ video-processing-service/src/processing/worker.py
@@ -1,8 +1,13 @@
 from redis import Redis
+from processing import process_video
 
 r = Redis(host='localhost', port=6379, decode_responses=True)
 
-while True:
-    print(r.brpop("video_details")[1])
+pubsub = r.pubsub()
+pubsub.subscribe('video_upload')
+
+for message in pubsub.listen():
+    if message['type'] == 'message':
+        process_video(r.brpop("video_details")[1])
 
 
