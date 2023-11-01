d41b65d6c72cf1605cfe250d86f7d6baed575c45
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-11-01 21:02:38 +0700

    finish wokers' function

--- video-processing-service/src/processing/thumbnail_worker.py
+++ video-processing-service/src/processing/thumbnail_worker.py
@@ -8,23 +8,32 @@ redis_conn = Redis(host='localhost', port=6379)
 
 q = Queue('thumbnail_generation', connection=redis_conn)
 
-app = Flask(__name__)
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='app.log',
+                    filemode='a')
+
 
 THUMBNAIL_DIR = 'thumbnails'
 
 if not os.path.exists(THUMBNAIL_DIR):
     os.makedirs(THUMBNAIL_DIR)
 
-def generate_thumbnail(video_file):
-    if video_file:
-        thumbnail_path = os.path.join(THUMBNAIL_DIR, video_file.replace('.mp4', '.jpg'))
-        subprocess.run(['ffmpeg', '-i', video_file.filename, '-ss', '00:00:10', '-vframes', '1', '-q:v', '2', thumbnail_path])
+def generate_thumbnail(video_filename):
+    if os.path.exists(video_filename):
+        logging.info(f"Started processing thumbnail job for video: {video_filename}")
+        thumbnail_path = os.path.join(THUMBNAIL_DIR, os.path.basename(video_filename).replace('.mp4', '.jpg'))
+        subprocess.run(['ffmpeg', '-i', video_filename, '-ss', '00:00:10', '-vframes', '1', '-q:v', '2', thumbnail_path])
 
+        logging.info(f"Completed processing thumbnail job for video: {video_filename}")
         return 'Thumbnail generation complete', 'thumbnail_path ' + thumbnail_path
     else:
         return 'Video file missing'
 
 if __name__ == '__main__':
     with Connection(redis_conn):
+        print('Video Thumbnail Worker is running...')
         worker = Worker(['thumbnail_generation'])
         worker.work()
